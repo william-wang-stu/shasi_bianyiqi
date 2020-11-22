cc3889344707bc1db663e92efbb8bbdf7a5c4cf4
william-wang-stu <2625421542@qq.com>
2020-11-22 17:31:14 +0800

    add notation_removal tests

--- notation_removal.py
+++ notation_removal.py
@@ -4,7 +4,7 @@
 # ret      dst_code     has been solved
 
 
-#update the lock area
+#update the lock area    -update the error-found    -correct the problem of wrong position in real file
 
 def notation_removal(src_code):
     #remove the space and \t head and tail
@@ -13,6 +13,9 @@ def notation_removal(src_code):
     dst_code=''
     i=0
     lock=0                #use to make sure the lock area
+    row=1                 #define the count of row
+    num_enter=0           #use to record the number of \n
+    flag=False                #use to symbolize whether find the */ or not
     while(i<code_len):
         if(i<code_len-1 and i>=lock):
             if(src_code[i]=='/' and src_code[i+1]=='/'):
@@ -29,13 +32,25 @@ def notation_removal(src_code):
                 else:
                     while (i<=code_len-2 and not(src_code[i]=='*' and src_code[i+1]=='/')):
                         i+=1
+                        if(src_code[i]=='\n'):
+                            num_enter+=1
                     if(i==code_len-1):
                         lock=i
                         i=point      #hasn't found the */ ,use pointer to recover
                     else:
                         i+=2
+                        flag=True
+        if(flag==True):             #find the right match for /* ,update the row and dst_str
+            dst_code+=num_enter*'\n'
+            row+=num_enter
+            num_enter=0
+
+        if(i<code_len-1 and ((src_code[i]=='*' and src_code[i+1]=='/') or (src_code[i]=='/' and src_code[i+1]=='*'))):
+            print('Error! illegal comment on row {}'.format(row))
         if(i<code_len):                
             dst_code+=src_code[i]
+            if(src_code[i]=='\n'):
+                row+=1
         i+=1                        
 
     return dst_code
--- test_interpreter.py
+++ test_interpreter.py
@@ -1,4 +1,68 @@
 import unittest
+import sys
+import io
+
+def stub_stdout(testcase_inst):
+    stderr = sys.stderr
+    stdout = sys.stdout
+
+    def cleanup():
+        sys.stderr = stderr
+        sys.stdout = stdout
+ 
+    testcase_inst.addCleanup(cleanup)
+    sys.stderr = io.StringIO()
+    sys.stdout = io.StringIO()
+
+class LexerTestCase(unittest.TestCase):
+    def buildLexer(self, text):
+        from Lexer import Lexer
+        from Parser import Parser
+        lexer = Lexer(text)
+        return lexer
+    
+    def test_notation_removal(self):
+        testcase = [
+            (
+                '''
+void main(void)
+{
+/*	int a
+	int b;
+	int c;
+	a = 3;
+	b=4;
+	c=2;
+	a=program(a,b,demo(c));
+	return ;
+}
+                ''',
+                "Error! illegal comment on row 4\n"
+            ),
+            (
+                '''
+void main(void)
+{
+/*	int a
+	int b;*/
+	int c;*/
+	a = 3;
+	b=4;
+	c=2;
+	a=program(a,b,demo(c));
+	return ;
+}
+                ''',
+                "Error! illegal comment on row 6\n"
+            )
+        ]
+        
+        from notation_removal import notation_removal
+        for text, error in testcase:
+            stub_stdout(self)
+            text = notation_removal(text)
+            self.assertEqual(str(sys.stdout.getvalue()), error)
+
 
 class ParserTestCase(unittest.TestCase):
     def buildParser(self, text):
